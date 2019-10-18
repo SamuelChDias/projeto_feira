c8879e5c545e2c5e2281d9d9a55c2c113f5b29e8
SamuelChDias <55776708+SamuelChDias@users.noreply.github.com>
2019-10-18 13:23:59 -0300

    Add files via upload

---
+++ Projeto-Feira(att20000)/README.md
@@ -0,0 +1,2 @@
+# Projeto-Feira
+Projeto da Feira cultural da escola Horácio Augusto Silveira (Centro Paula Souza)
---
+++ Projeto-Feira(att20000)/_js/funcoes.js
@@ -0,0 +1,39 @@
+function receberValores(receberKill,receberDeaths,receberAssists){
+	var kill = parseFloat(receberKill);
+	var assists = parseFloat(receberAssists);
+	var deaths = parseFloat(receberDeaths);
+
+	var resultado = (kill + assists)/deaths
+
+	txtResultado.value = parseFloat(resultado);
+	//((kill + assists)/deaths);
+}
+
+/* configuração do carrossel */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
